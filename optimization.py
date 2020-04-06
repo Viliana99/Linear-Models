40127b8df22c4805d57238dd119f5c3e3887ec8c
Viliana Devbunova <55013584+Viliana99@users.noreply.github.com>
2020-04-07 01:15:23 +0300

    formatting

--- optimization.py
+++ optimization.py
@@ -4,104 +4,42 @@ import time
 
 
 class GDClassifier:
-
     """
-
     Реализация метода градиентного спуска для произвольного
-
     оракула, соответствующего спецификации оракулов из модуля oracles.py
-
     """
-
-    
-
-    def __init__(self, loss_function, step_alpha=1, step_beta=0, 
-
-                 tolerance=1e-5, max_iter=1000, **kwargs):
-
+    def __init__(self, loss_function, step_alpha=1, step_beta=0, tolerance=1e-5, max_iter=1000, **kwargs):
         """
-
         loss_function - строка, отвечающая за функцию потерь классификатора. 
-
         Может принимать значения:
-
         - 'binary_logistic' - бинарная логистическая регрессия
-
-                
-
-        step_alpha - float, параметр выбора шага из текста задания
-
-
-        
-
-        step_beta- float, параметр выбора шага из текста задания
-
-        
-
-        tolerance - точность, по достижении которой, необходимо прекратить оптимизацию.
-
-        Необходимо использовать критерий выхода по модулю разности соседних значений функции:
-
+        step_alpha - float, параметр выбора шага 
+        step_beta- float, параметр выбора шага 
+        tolerance - точность, по достижении которой, прекращаем оптимизацию.
+       Критерий выхода по модулю разности соседних значений функции:
         если |f(x_{k+1}) - f(x_{k})| < tolerance: то выход 
-
-        
-
         max_iter - максимальное число итераций     
-
-        
-
         **kwargs - аргументы, необходимые для инициализации   
-
         """
-
-        #self.loss_function = loss_function
         self.step_alpha = step_alpha
         self.step_beta = step_beta
         self.tolerance = tolerance
         self.max_iter = max_iter
         self.oracle = oracles.BinaryLogistic(**kwargs)
-        
-
-        
 
     def fit(self, X, y, w_0=None, trace=False):
-
         """
-
         Обучение метода по выборке X с ответами y
-
-        
-
         X - scipy.sparse.csr_matrix или двумерный numpy.array
-
-        
-
         y - одномерный numpy array
-
-        
-
         w_0 - начальное приближение в методе
-
-        
-
         trace - переменная типа bool
-
-      
-
         Если trace = True, то метод должен вернуть словарь history, содержащий информацию 
-
         о поведении метода. Длина словаря history = количество итераций + 1 (начальное приближение)
-
-        
-
         history['time']: list of floats, содержит интервалы времени между двумя итерациями метода
-
         history['func']: list of floats, содержит значения функции на каждой итерации
-
         (0 для самой первой точки)
-
         """
-
         w = w_0
         last_func = self.oracle.func(X, y, w)
         if trace:
@@ -120,176 +58,74 @@ class GDClassifier:
         self.weights = w
         if trace:
             return history
-            
-        
-
+          
     def predict(self, X):
-
         """
-
         Получение меток ответов на выборке X
-
-        
-
         X - scipy.sparse.csr_matrix или двумерный numpy.array
-
-        
-
         return: одномерный numpy array с предсказаниями
-
         """
-    
         res = X.dot(self.weights)
         pred = np.ones_like(res)
         pred[np.where(res < 0)[0]] = -1
         return pred.astype(int)
 
-
-
     def predict_proba(self, X):
-
         """
-
         Получение вероятностей принадлежности X к классу k
-
-        
-
         X - scipy.sparse.csr_matrix или двумерный numpy.array
-
-        
-
         return: двумерной numpy array, [i, k] значение соответветствует вероятности
-
         принадлежности i-го объекта к классу k 
-
         """
-
         p = expit(X.dot(self.weights))
-        return np.array([p , 1 - p]).T
-
-        
+        return np.array([p , 1 - p]).T     
 
     def get_objective(self, X, y):
-
         """
-
         Получение значения целевой функции на выборке X с ответами y
-
-        
-
         X - scipy.sparse.csr_matrix или двумерный numpy.array
-
         y - одномерный numpy array
-
-        
-
         return: float
-
         """
-
-        return self.oracle.func(X, y, self.weights)
-
-        
+        return self.oracle.func(X, y, self.weights) 
 
     def get_gradient(self, X, y):
-
         """
-
         Получение значения градиента функции на выборке X с ответами y
-
-        
-
         X - scipy.sparse.csr_matrix или двумерный numpy.array
-
         y - одномерный numpy array
-
-        
-
         return: numpy array, размерность зависит от задачи
-
         """
-
         return self.oracle(X, y, self.weights)
 
-    
-
     def get_weights(self):
-
         """
-
         Получение значения весов функционала
-
         """    
-
         return self.weights
 
 
-
-
-
 class SGDClassifier(GDClassifier):
-
     """
-
     Реализация метода стохастического градиентного спуска для произвольного
-
     оракула, соответствующего спецификации оракулов из модуля oracles.py
-
     """
-
-    
-
-    def __init__(self, loss_function, batch_size, step_alpha=1, step_beta=0, 
-
-                 tolerance=1e-5, max_iter=1000, random_seed=153, **kwargs):
-
-        """
-
+    def __init__(self, loss_function, batch_size, step_alpha=1, step_beta=0, tolerance=1e-5, max_iter=1000, random_seed=153, **kwargs):
+       """
         loss_function - строка, отвечающая за функцию потерь классификатора. 
-
         Может принимать значения:
-
         - 'binary_logistic' - бинарная логистическая регрессия
-
-        
-
-        batch_size - размер подвыборки, по которой считается градиент
-
-        
-
-        step_alpha - float, параметр выбора шага из текста задания
-
-        
-
-        step_beta- float, параметр выбора шага из текста задания
-
-        
-
+        batch_size - размер подвыборки, по которой считается градиент        
+        step_alpha - float, параметр выбора шага
+        step_beta- float, параметр выбора шага
         tolerance - точность, по достижении которой, необходимо прекратить оптимизацию
-
-        Необходимо использовать критерий выхода по модулю разности соседних значений функции:
-
+        Критерий выхода по модулю разности соседних значений функции:
         если |f(x_{k+1}) - f(x_{k})| < tolerance: то выход 
-
-        
-
-        
-
         max_iter - максимальное число итераций (эпох)
-
-        
-
         random_seed - в начале метода fit необходимо вызвать np.random.seed(random_seed).
-
         Этот параметр нужен для воспроизводимости результатов на разных машинах.
-
-        
-
         **kwargs - аргументы, необходимые для инициализации
-
         """
-        
-        #self.loss_function = loss_function
         self.batch_size = batch_size
         self.step_alpha = step_alpha
         self.step_beta = step_beta
@@ -297,65 +133,29 @@ class SGDClassifier(GDClassifier):
         self.max_iter = max_iter
         self.random_seed = random_seed
         self.oracle = oracles.BinaryLogistic(**kwargs)
-        
-
-        
 
     def fit(self, X, y, w_0=None, trace=False, log_freq=1):
-
         """
-
         Обучение метода по выборке X с ответами y
-
-        
-
         X - scipy.sparse.csr_matrix или двумерный numpy.array
-
-        
-
         y - одномерный numpy array
-
-                
-
         w_0 - начальное приближение в методе
-
-        
-
-        Если trace = True, то метод должен вернуть словарь history, содержащий информацию 
-
+        Если trace = True, то метод  возвращают словарь history, содержащий информацию 
         о поведении метода. Если обновлять history после каждой итерации, метод перестанет 
-
         превосходить в скорости метод GD. Поэтому, необходимо обновлять историю метода лишь
-
         после некоторого числа обработанных объектов в зависимости от приближённого номера эпохи.
-
         Приближённый номер эпохи:
-
             {количество объектов, обработанных методом SGD} / {количество объектов в выборке}
-
-        
-
         log_freq - float от 0 до 1, параметр, отвечающий за частоту обновления. 
-
         Обновление должно проиходить каждый раз, когда разница между двумя значениями приближённого номера эпохи
-
         будет превосходить log_freq.
-
-        
-
         history['epoch_num']: list of floats, в каждом элементе списка будет записан приближённый номер эпохи:
-
         history['time']: list of floats, содержит интервалы времени между двумя соседними замерами
-
         history['func']: list of floats, содержит значения функции после текущего приближённого номера эпохи
-
         history['weights_diff']: list of floats, содержит квадрат нормы разности векторов весов с соседних замеров
-
         (0 для самой первой точки)
-
         """
         np.random.seed(self.random_seed)
-
         w = w_0
         step = 1
         view_item = 0
@@ -379,7 +179,6 @@ class SGDClassifier(GDClassifier):
             w = w - nu_step * self.oracle.grad(X[index], y[index], w)
             new_func = self.oracle.func(X, y, w)
             if trace:
-                #print('===', view_item / X.shape[0] - epoch)
                 if (view_item / X.shape[0] - epoch) > log_freq:
                     epoch = view_item / X.shape[0]
                     history['time'].append(time.time() - start_time)
--- oracles.py
+++ oracles.py
@@ -3,88 +3,42 @@ import scipy
 from scipy.special import expit 
 from scipy.sparse import csr_matrix
 
+
 class BaseSmoothOracle:
-
     """
-
     Базовый класс для реализации оракулов.
-
     """
-
     def func(self, w):
-
         """
-
-        Вычислить значение функции в точке w.
-
+        Вычисление значения функции в точке w.
         """
-
         raise NotImplementedError('Func oracle is not implemented.')
 
-
-
     def grad(self, w):
-
         """
-
-        Вычислить значение градиента функции в точке w.
-
+         Вычисление значения градиента функции в точке w.
         """
-
         raise NotImplementedError('Grad oracle is not implemented.')
-
-
-
-        
-
-class BinaryLogistic(BaseSmoothOracle):
-
-    """
-
-    Оракул для задачи двухклассовой логистической регрессии.
-
-    
-
-    Оракул должен поддерживать l2 регуляризацию.
-
-    """
-
-    
-
-    def __init__(self, l2_coef):
-
-        """
-
-        Задание параметров оракула.
-
-        
-
-        l2_coef - коэффициент l2 регуляризации
-
-        """
-        
-        self.l2_coef = l2_coef
-
      
 
-    def func(self, X, y, w=None):
-
+class BinaryLogistic(BaseSmoothOracle):
+    """
+    Оракул для задачи двухклассовой логистической регрессии.
+    Оракул поддерживает l2 регуляризацию.
+    """
+    def __init__(self, l2_coef):
         """
+        Задание параметров оракула
+        l2_coef - коэффициент l2 регуляризации
+        """
+        self.l2_coef = l2_coef
 
-        Вычислить значение функционала в точке w на выборке X с ответами y.
-
-        
-
+    def func(self, X, y, w=None):
+        """
+         Вычисление значения функционала в точке w на выборке X с ответами y.
         X - scipy.sparse.csr_matrix или двумерный numpy.array
-
-        
-
         y - одномерный numpy array
-
-        
-
         w - одномерный numpy array
-
         """
         if w is None:
             w = np.zeros(X.shape[1])  
@@ -93,26 +47,12 @@ class BinaryLogistic(BaseSmoothOracle):
         loss = np.logaddexp(0, marg).mean()
         return loss + reg
 
-        
-
     def grad(self, X, y, w=None):
-
         """
-
-        Вычислить градиент функционала в точке w на выборке X с ответами y.
-
-        
-
+        Вычисление градиента функционала в точке w на выборке X с ответами y.
         X - scipy.sparse.csr_matrix или двумерный numpy.array
-
-        
-
         y - одномерный numpy array
-
-        
-
         w - одномерный numpy array
-
         """
         if w is None:
             w = np.zeros(X.shape[1])
